f0cad5de42c92fb43de8fa76031a13a7e2e8a13a
ThiwankaShan <shane.thiwanka@gmail.com>
2022-07-07 15:54:20 +0530

    add admin seed script

---
+++ server/scripts/seed.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const User = require('../models/user.model');
+require('dotenv').config();
+
+const data = {
+    'id': 0,
+    'email': 'admin@note.com',
+    'password': 'admin123',
+    'firstName': 'admin',
+    'lastName': 'admin',
+    'dateOfBirth': '1990-01-01',
+    'mobile': 0,
+    'accountType': 'admin',
+    'status': true
+}
+
+seed = async ()=>{
+    try{
+        await User.create(data);
+        console.log('✅ Admin account created successfully \n');
+        process.exit();
+    }catch(e){
+        console.log('❌ Something went wrong (refer error below)');
+        console.log(e);
+        process.exit();
+    }
+    
+}
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("✅ Connected to MongoDB server \n");
+        seed();
+    })
+    .catch((error) => {
+        console.log(`error === > ${error}`);
+    });
